a8885790d1d238b7a95fe8660a83dc4884f61485
JumpingMonkey <morozovprav@gmail.com>
2022-09-14 10:57:02 +0100

    add seeder. add state to user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,4 +30,20 @@ class UserFactory extends Factory
             'remember_token' => Str::random(10),
         ];
     }
+
+    /**
+     * Indicate that the user is suspended.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function johnDoe()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'name' => 'Jonny Doe',
+                'email' => 'test@mail',
+                'password' => '$2y$10$5Qz0XGPkDLpS0YvfQCdDCOT/y9oAibb80y.JnxdRZZUpRwZb5jw2O',
+            ];
+        });
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::factory()->johnDoe()->create();
+        User::factory(10)->create();
     }
 }
