f707e71b87083268d4fb46fe2c59e76d7a262c1e
JumpingMonkey <morozovprav@gmail.com>
2022-09-18 21:42:29 +0100

    add post auhor to blogost index

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,16 +3,7 @@
 @section('title', 'Blog posts')
 
 @section('content')
-{{--    @if(count($posts))--}}
-{{--        @foreach($posts as $key => $post)--}}
-{{--            <div>{{ $key }}.{{$post['title']}}</div>--}}
-{{--        @endforeach--}}
-{{--    @else--}}
-{{--        <div>No posts found!</div>--}}
-{{--    @endif--}}
-{{--alternative--}}
 
-{{--@each('posts.partials.post', $posts, 'post')--}}
 @forelse($posts as $key => $post)
     @include('posts.partials.post', [])
 @empty
