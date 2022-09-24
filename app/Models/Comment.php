84072226a80da8daca5a578064962680d9189f65
morozov <morozovprav@gmail.com>
2022-09-24 20:08:40 +0100

    add global scope to comment

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Scopes\LatestScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -17,5 +18,12 @@ class Comment extends Model
     public function blogPost()
     {
         return $this->belongsTo(BlogPost::class);
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new latestScope);
     }
 }
