54bd6b99fceada2dc76bb91e6535da3b7686c14e
morozov <morozovprav@gmail.com>
2022-11-19 21:37:38 +0000

    update image model and show post html

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Image extends Model
 {
@@ -17,5 +18,10 @@ class Image extends Model
     public function blogPost(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
         return $this->belongsTo(BlogPost::class);
+    }
+
+    public function url()
+    {
+        return Storage::url($this->path);
     }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,14 +5,28 @@
 @section('content')
     <div class="row">
         <div class="col-8">
-<h1>
+            @if($post->image)
+            <div class="" style="background-image: url('{{ $post->image->url() }}'); min-height: 500px; color:white; text-align: center; background-attachment: fixed;">
+                <h1 style="padding-top: 100px; text-shadow: 1px 2px #000">
+            @else
+                <h1>
+            @endif
     {{$post->title}}
         @component('components.badge', ['show' => now()->diffInMinutes($post->created_at) < 30])
             New!
         @endcomponent
-</h1>
+            @if($post->image)
+                </h1>
+            </div>
+            @else
+            </h1>
+            @endif
 <p>{{$post->content}}</p>
 {{--<p>Added {{ $post->created_at->diffForHumans() }}</p>--}}
+
+{{--<img src="http://cource/storage/{{ $post->image->path }}"/>--}}
+{{--<img src="{{ Storage::url($post->image->path) }}"/>--}}
+
 
 @component('components.updated', ['date' => $post->created_at, 'name' => $post->user->name])
 
