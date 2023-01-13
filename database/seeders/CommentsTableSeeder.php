fa91735294a45c90ee61c1c3e97fb881551a1985
morozov <morozovprav@gmail.com>
2023-01-13 21:49:25 +0000

    update comment table seeder

--- database/seeders/CommentsTableSeeder.php
+++ database/seeders/CommentsTableSeeder.php
@@ -17,21 +17,28 @@ class CommentsTableSeeder extends Seeder
     public function run()
     {
         $posts = BlogPost::all();
+        $users = User::all();
 
-        if ($posts->count() === 0)
+        if ($posts->count() === 0 OR $users->count() === 0)
         {
-            $this->command->info('There are no blog posts, so no comments will be added!');
+            $this->command->info('There are no blog posts or users, so no comments will be added!');
             return;
         }
 
         $commentCount = (int)$this->command->ask('How many comments would you like?', 150);
 
-        $users = User::all();
-
         Comment::factory($commentCount)->make()->each(function ($comment) use($posts, $users){
-        $comment->blog_post_id = $posts->random()->id;
-        $comment->user_id = $users->random()->id;
-        $comment->save();
+            $comment->commentable_id = $posts->random()->id;
+            $comment->commentable_type = BlogPost::class;
+            $comment->user_id = $users->random()->id;
+            $comment->save();
+        });
+
+        Comment::factory($commentCount)->make()->each(function ($comment) use($users){
+            $comment->commentable_id = $users->random()->id;
+            $comment->commentable_type = User::class;
+            $comment->user_id = $users->random()->id;
+            $comment->save();
         });
     }
 }
