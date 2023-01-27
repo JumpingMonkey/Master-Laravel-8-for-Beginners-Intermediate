cbd9fc4d49311db18c7b57fc68e951c717fc8d8c
morozov <morozovprav@gmail.com>
2023-01-27 21:55:49 +0000

    add event and listener

--- app/Http/Controllers/PostCommentController.php
+++ app/Http/Controllers/PostCommentController.php
@@ -2,13 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\CommentPosted;
 use App\Http\Requests\StoreComment;
-use App\Jobs\NotifyUsersPostWasCommented;
-use App\Jobs\ThrottledMail;
-use App\Mail\CommentPosted;
-use App\Mail\CommentPostedMarkdown;
 use App\Models\BlogPost;
-use Illuminate\Support\Facades\Mail;
 
 class PostCommentController extends Controller
 {
@@ -24,25 +20,7 @@ class PostCommentController extends Controller
             'user_id' => $request->user()->id
         ]);
 
-//        Mail::to($post->user)->send(
-//            new CommentPostedMarkdown($comment)
-//        );
-//        $when = now()->addMinutes(1);
-
-//        Mail::to($post->user)->queue(
-//            new CommentPostedMarkdown($comment)
-//        );
-
-        ThrottledMail::dispatch(new CommentPostedMarkdown($comment), $post->user)
-            ->onQueue('low');
-
-        NotifyUsersPostWasCommented::dispatch($comment)
-            ->onQueue('high');
-
-//        Mail::to($post->user)->later(
-//            $when,
-//            new CommentPostedMarkdown($comment)
-//        );
+        event(new CommentPosted($comment));
 
         return redirect()->back()
             ->withStatus('Comment was created!');
