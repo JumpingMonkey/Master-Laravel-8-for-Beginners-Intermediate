f4495167401142b7ccc42e01e262903308a096f3
morozov <morozovprav@gmail.com>
2023-01-28 20:24:02 +0000

    add basic translations

---
+++ resources/lang/es/messages.php
@@ -0,0 +1,7 @@
+<?php
+
+    return [
+        'welcome' => 'Bienvenido a Laravel!',
+        'example' => 'Ola :name',
+        'plural' => '{0} No commentarios :a|{1} :count commentario :a|[2,*] :count commentarios :a'
+    ];
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,6 +3,11 @@
 @section('title', 'Home Page')
 
 @section('content')
-    <h1>Home Page</h1>
+    <h1>{{ __('messages.welcome') }}</h1>
+    <h1>@lang('messages.welcome')</h1>
+
+    <p>{{__('messages.example', ['name' => 'Alex'])}}</p>
+    <p>{{trans_choice('messages.plural', 2, ['a' => 1])}}</p>
+
     <p>Home page content!</p>
 @endsection
