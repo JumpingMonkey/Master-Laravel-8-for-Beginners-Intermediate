9291e74cdedef2a3c00670830778bc70955bd7b9
morozov <morozovprav@gmail.com>
2023-03-05 22:52:53 +0000

    test update

---
+++ tests/Feature/ApiPostCommentsTest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\BlogPost;
+use App\Models\Comment;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ApiPostCommentsTest extends TestCase
+{
+
+    use RefreshDatabase;
+    public function testNewBlogPostDoesNotHaveComments()
+    {
+        $this->blogPost();
+
+        $response = $this->getJson('api/v1/posts/1/comments');
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(['data', 'links', 'meta'])
+            ->assertJsonCount(0, 'data');
+    }
+
+    public function testBlogPostHas10Comments()
+    {
+        $this->blogPost()->each(function ($post){
+            $post->comments()->saveMany(Comment::factory()
+                ->count(10)
+                ->make([
+                'user_id' => $this->user()->id
+                ])
+            );
+        });
+
+        $response = $this->getJson('api/v1/posts/2/comments');
+
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => [
+                        'id',
+                        'content',
+                        'created_at',
+                        'updated_at',
+                        'user' => [
+                            'id',
+                            'name'
+                        ]
+                    ]
+                ],
+                'links',
+                'meta'
+            ])
+            ->assertJsonCount(10, 'data');
+    }
+
+    public function testAddingCommentsWhenNotAuthenticated()
+    {
+        $this->blogPost();
+
+        $response = $this->postJson('api/v1/posts/3/comments',[
+            'content' => 'Hello',
+        ]);
+
+        $response->assertStatus(401);
+    }
+
+    public function testAddingCommentWhenAuthenticated()
+    {
+        $this->blogPost();
+
+        $response = $this->actingAs($this->user(), 'api')
+            ->postJson('api/v1/posts/4/comments',[
+            'content' => 'Hello',
+        ]);
+
+        $response->assertStatus(201);
+    }
+
+    public function testAddingCommentWithInvalidData()
+    {
+        $this->blogPost();
+
+        $response = $this->actingAs($this->user(), 'api')
+            ->postJson('api/v1/posts/5/comments',[
+                'content' => 'He',
+            ]);
+
+        $response->assertStatus(422)
+            ->assertJson([
+                "message" => "The given data was invalid.",
+                "errors" => [
+                    "content" => [
+                        "The content must be at least 5 characters."
+                    ]
+                ]
+            ]);
+    }
+}
